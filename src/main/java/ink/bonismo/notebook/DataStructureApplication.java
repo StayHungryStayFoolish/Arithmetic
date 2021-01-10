63880f7153a3465e80618ae110399e3c615900bd
Bonismo <bonismo@hotmail.com>
2021-01-10 23:13:58 +0800

    standardizing package naming

--- src/main/java/ink/bonismo/notebook/DataStructureApplication.java
+++ src/main/java/ink/bonismo/notebook/DataStructureApplication.java
@@ -1,4 +1,4 @@
-package ink.bonismo.notebook.arithmetic.sort;
+package ink.bonismo.notebook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
