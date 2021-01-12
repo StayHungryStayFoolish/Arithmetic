d92046b03ac0dc2ef768f9dcd9e8e8d3f428afc7
Bonismo <bonismo@hotmail.com>
2021-01-12 17:57:10 +0800

    minor change

--- src/main/java/ink/bonismo/notebook/arithmetic/data_structure/SingleLinkedList.java
+++ src/main/java/ink/bonismo/notebook/arithmetic/data_structure/SingleLinkedList.java
@@ -2,46 +2,136 @@ package ink.bonismo.notebook.arithmetic.data_structure;
 
 import org.w3c.dom.Node;
 
-import java.util.LinkedList;
-
-public class SingleLinkedList {
+public class SingleLinkedList<E> {
 
     private int size;
-    private Node first;
+    private Node<E> first;
 
     public SingleLinkedList() {
     }
 
-    public  void addFirst(Object val) {
-        addIndex(0, val);
+    public void add(E val) {
+        addTail(val);
     }
 
-    public  void addTail(Object val) {
-        addIndex(size, val);
+    public boolean remove(Object o) {
+        for (int i = 0; i < size; i++) {
+            Node<E> result = findNodeAtIndex(i);
+            if (result != null) {
+                if (o == result.data) {
+                    deleteAtIndex(i);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
-    private void addIndex(int index, Object val) {
-        if (index <= 0) {
-            first = new Node(val, first);
+    public void addFirst(E val) {
+        addAtIndex(0, val);
+    }
+
+    public void addTail(E val) {
+        addAtIndex(size, val);
+    }
+
+    public void deleteAtIndex(int index) {
+        if (!rangeCheck(index)) {
+            return;
+        }
+        if (index == 0) {
+            first = first.next;
+        } else {
+            Node<E> prev = findNodeAtIndex(index - 1);
+            if (prev == null) {
+                return;
+            }
+            prev.next = prev.next.next;
+            size--;
         }
     }
 
-    private boolean checkIndex(int index) {
+    private void addAtIndex(int index, E val) {
+        if (index <= 0) {
+            first = new Node<>(val, first);
+        } else {
+            Node prev = findNodeAtIndex(index - 1);
+            if (prev == null) {
+                return;
+            }
+            prev.next = new Node<>(val, prev.next);
+        }
+        size++;
+    }
+
+    public String toString() {
+        StringBuffer result = new StringBuffer();
+        Node<E> node = first;
+        for (int i = 0; i < size; i++) {
+            if (i == 0) {
+                node = first;
+            } else {
+                node = node.next;
+            }
+            if (i != size - 1) {
+                result.append(node.data).append(",");
+            } else {
+                result.append(node.data);
+            }
+        }
+        return result.toString();
+    }
+
+    private boolean rangeCheck(int index) {
         return !(index < 0 || index > size - 1);
     }
 
 
-    private class Node{
-        private Object data;
-        private Node next;
+    private Node<E> findNodeAtIndex(int index) {
+        if (!rangeCheck(index)) {
+            return null;
+        }
+        Node<E> node = first;
+        for (int i = 0; i < index; i++) {
+            node = node.next;
+        }
+        return node;
+    }
 
-        public Node(Object data) {
+
+    private static class Node<E> {
+        private E data;
+        private Node<E> next;
+
+        public Node(E data) {
             this.data = data;
         }
 
-        public Node(Object data, Node next) {
+        public Node(E data, Node next) {
             this(data);
             this.next = next;
         }
+    }
+
+    public static void main(String[] args) {
+        SingleLinkedList<String> list = new SingleLinkedList<>();
+        list.addTail("a");
+        list.addTail("f");
+        list.addTail("b");
+        list.addTail("f");
+        list.addTail("e");
+
+        System.out.println(list.toString());
+        list.remove("f");
+        System.out.println(list.toString());
+
+        SingleLinkedList<Integer> linkedList = new SingleLinkedList<>();
+        linkedList.addTail(1);
+        linkedList.addTail(2);
+        linkedList.addTail(3);
+        linkedList.addTail(4);
+        linkedList.addTail(5);
+
+        System.out.println(linkedList.toString());
     }
 }
